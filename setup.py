6e70c2e3340082e6593b5c035ceac2566407b063
Jack Stephenson <jack@bancast.net>
2016-04-27 11:17:33 +0100

    Updated version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='cloi',
-    version='0.5.0',
+    version='0.6.0',
     description='A connector to implement connecting to CLOI-based instruments',
     author='Xtralien',
     author_email='jack@xtralien.com',
