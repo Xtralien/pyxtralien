f33ecb5cc00f86ef253ccc6a8a88f3960070bc2d
Chris Bracher.pub <c.bracher@ossila.com>
2022-12-19 14:38:39 +0000

    Add license, toml file, and update setup.py

--- setup.py
+++ setup.py
@@ -8,11 +8,11 @@ except:
 
 setup(
     name='xtralien',
-    version='2.9.1',
+    version='2.10.0',
     description='A connector to implement connecting to CLOI-based instruments',
     long_description=long_description,
-    author='Xtralien',
-    author_email='jack@xtralien.com',
+    author='Ossila Ltd.',
+    author_email='info@ossila.com',
     url='https://github.com/xtralien/pyxtralien.git',
     packages=['xtralien'],
     extras_require={
@@ -20,5 +20,5 @@ setup(
         'keithley': ['pyVISA'],
         'prompt': ['pyreadline']
     },
-    license='GPLv3'
+    license='MIT'
 )
